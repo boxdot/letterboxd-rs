13a658a0c5a132df6ed314e65beab93d5eab9867
boxdot <d@zerovolt.org>
2017-09-06 21:05:34 +0200

    Implement generic GET and PATCH macros.
    * Use macros for search, lists, and patch list endpoints.
    * Make examples more generic taking parameters.
    * Add example for search.

--- examples/update_list.rs
+++ examples/update_list.rs
@@ -2,19 +2,28 @@ extern crate futures;
 extern crate letterboxd;
 extern crate tokio_core;
 
+use std::env;
+use std::process;
+
 use futures::Future;
 use tokio_core::reactor::Core;
 
-const LETTERBOXD_API_KEY: &'static str = "your_key";
-const LETTERBOXD_API_SECRET: &'static str = "your_secret";
-const USERNAME: &'static str = "your_username";
-const PASSWORD: &'static str = "your_password";
+fn exit_with_usage() -> String {
+    println!("Usage: example <api_key> <api_secret> <username> <password> <listid>");
+    process::exit(1);
+}
 
 fn main() {
+    let mut args = env::args();
+    args.next();
+    let api_key = args.next().unwrap_or_else(exit_with_usage);
+    let api_secret = args.next().unwrap_or_else(exit_with_usage);
+    let username = args.next().unwrap_or_else(exit_with_usage);
+    let password = args.next().unwrap_or_else(exit_with_usage);
+    let lid = args.next().unwrap_or_else(exit_with_usage);
+
     let mut core = Core::new().unwrap();
-    let key = String::from(LETTERBOXD_API_KEY);
-    let secret = String::from(LETTERBOXD_API_SECRET);
-    let client = letterboxd::Client::new(&core.handle(), key, secret);
+    let client = letterboxd::Client::new(&core.handle(), api_key, api_secret);
 
     let do_patch = |token| {
         let mut req = letterboxd::ListUpdateRequest::new(String::from("testing_list"));
@@ -24,7 +33,7 @@ fn main() {
             ];
 
         // Letterboxd list id `LID` can be found on the share button
-        client.patch_list("LID", &req, &token)
+        client.patch_list(&lid, &req, &token)
     };
 
     let do_print = |resp| {
@@ -32,7 +41,7 @@ fn main() {
         Ok(())
     };
 
-    let do_auth = client.auth(USERNAME, PASSWORD);
+    let do_auth = client.auth(&username, &password);
     core.run(do_auth.and_then(do_patch).and_then(do_print))
         .unwrap();
 }
