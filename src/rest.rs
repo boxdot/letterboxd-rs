d29cb6893859c771f6125c428eef34e4ddbfe6c3
boxdot <d@zerovolt.org>
2017-09-07 19:08:51 +0200

    Reorganize module structure.

--- src/rest.rs
+++ src/rest.rs
@@ -1,100 +1,115 @@
-#[macro_export]
-macro_rules! GET {
-    ($func_name:ident, ($path:expr $(, $arg:ident:$T:ty)*), $RequestType:ty, $ResponseType:tt) => {
-        pub fn $func_name(
-            &self $( , $arg: $T)*, request: &$RequestType, access_token: Option<&AccessToken>
-        ) -> Box<Future<Item = $ResponseType, Error = Error>> {
+use std::str;
 
-            let uri = self.generate_signed_url(
-                hyper::Method::Get,
-                &format!($path $(, $arg)*),
-                &serde_url_params::to_string(request).unwrap(), // TODO: Error handling
-                "",
-            );
-            let uri: hyper::Uri = match uri.parse() {
-                Ok(uri) => uri,
-                Err(err) => {
-                    return Box::new(future::result(Err(Error::from(err))));
-                }
-            };
+use futures::{Future, Stream, future};
+use hyper;
+use hyper_tls;
+use serde_json;
+use tokio_core::reactor::Handle;
+use uuid;
 
-            let mut req = hyper::Request::new(hyper::Method::Get, uri.clone());
-            req.headers_mut().set(hyper::header::ContentType::json());
-            req.headers_mut().set(hyper::header::ContentLength(0));
-            if let Some(token) = access_token {
-                req.headers_mut().set(hyper::header::Authorization(
-                    hyper::header::Bearer { token: token.access_token.clone() },
-                ));
-            };
+use defs;
+use error::Error;
+use helper;
 
-            let get = self.hyper_client.request(req).from_err();
-            let fut_resp = get.and_then(move |resp| {
-                let status_code = resp.status();
-                let body = resp.body().concat2().from_err();
-                body.and_then(move |chunk| if status_code != hyper::StatusCode::Ok {
-                    let resp = String::from(str::from_utf8(&chunk)?);
-                    Err(Error::server_error(status_code, resp, uri))
-                } else {
-                    let json: $ResponseType = serde_json::from_slice(&chunk)?;
-                    Ok(json)
-                })
-            });
-            Box::new(fut_resp)
-        }
-    };
-    ($func_name:ident, $path:expr, $ReqT:ty, $RespT:tt) => {
-        GET!($func_name, ($path), $ReqT, $RespT);
-    };
+pub struct Client {
+    url: String,
+    key: String,
+    shared_secret: String,
+    // state
+    hyper_client: hyper::Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>>,
 }
 
-#[macro_export]
-macro_rules! PATCH {
-    ($func_name:ident, ($path:expr $(, $arg:ident:$T:ty)*), $RequestType:ty, $ResponseType:tt) => {
-        pub fn $func_name(
-            &self $( , $arg: $T)*, request: &$RequestType, access_token: &AccessToken
-        ) -> Box<Future<Item = $ResponseType, Error = Error>> {
+impl Client {
+    pub fn new(handle: &Handle, api_key: String, api_shared_secret: String) -> Client {
+        let hyper_client = hyper::Client::configure()
+            .connector(hyper_tls::HttpsConnector::new(4, handle).unwrap())
+            .build(handle);
+        Client {
+            url: String::from("https://letterboxd.com/api/v0/"),
+            key: api_key,
+            shared_secret: api_shared_secret,
+            hyper_client: hyper_client,
+        }
+    }
 
-            let body = match serde_json::to_string(request) {
-                Ok(body) => body,
-                Err(err) => return Box::new(future::result(Err(Error::from(err)))),
-            };
-            let uri: hyper::Uri = match self.generate_signed_url(
-                hyper::Method::Patch,
-                &format!($path $(, $arg)*),
-                "",
-                &body,
-            ).parse() {
+    GET!(search, "search", defs::SearchRequest, defs::SearchResponse);
+    GET!(get_lists, "lists", defs::ListsRequest, defs::ListsResponse);
+    PATCH!(patch_list, ("list/{}", id: &str), defs::ListUpdateRequest, defs::ListUpdateResponse);
+
+    pub fn auth(
+        &self,
+        username: &str,
+        password: &str,
+    ) -> Box<Future<Item = defs::AccessToken, Error = Error>> {
+        let body = format!("grant_type=password&username={}&password={}", username, password);
+        let uri: hyper::Uri =
+            match self.generate_signed_url(hyper::Method::Post, "auth/token", "", &body)
+                .parse() {
                 Ok(uri) => uri,
                 Err(err) => return Box::new(future::result(Err(Error::from(err)))),
             };
-            println!("{:?}", uri);
 
-            let mut req = hyper::Request::new(hyper::Method::Patch, uri.clone());
-            req.headers_mut().set(hyper::header::ContentType::json());
-            req.headers_mut().set(hyper::header::ContentLength(
-                body.len() as u64,
-            ));
-            req.headers_mut().set(hyper::header::Authorization(
-                hyper::header::Bearer { token: access_token.access_token.clone() },
-            ));
-            req.set_body(body);
+        let mut req = hyper::Request::new(hyper::Method::Post, uri.clone());
+        req.headers_mut().set(
+            hyper::header::ContentType::form_url_encoded(),
+        );
+        req.headers_mut().set(hyper::header::Accept::json());
+        req.set_body(body);
 
-            let patch = self.hyper_client.request(req).from_err();
-            let fut_resp = patch.and_then(move |resp| {
-                let status_code = resp.status();
-                let body = resp.body().concat2().from_err();
-                body.and_then(move |chunk| if status_code != hyper::StatusCode::Ok {
-                    let resp = String::from(str::from_utf8(&chunk)?);
-                    Err(Error::server_error(status_code, resp, uri))
-                } else {
-                    let json: $ResponseType = serde_json::from_slice(&chunk)?;
-                    Ok(json)
-                })
-            });
-            Box::new(fut_resp)
-        }
-    };
-    ($func_name:ident, $path:expr, $ReqT:ty, $RespT:tt) => {
-        GET!($func_name, ($path), $ReqT, $RespT);
-    };
+        let req = self.hyper_client.request(req).from_err();
+        let fut_resp = req.and_then(move |resp| {
+            let status_code = resp.status();
+            let body = resp.body().concat2().from_err();
+            body.and_then(move |chunk| if status_code != hyper::StatusCode::Ok {
+                let resp = String::from(str::from_utf8(&chunk)?);
+                Err(Error::server_error(status_code, resp, uri))
+            } else {
+                let json: defs::AccessToken = serde_json::from_slice(&chunk)?;
+                println!("{:?}", json);
+                Ok(json)
+            })
+        });
+        Box::new(fut_resp)
+    }
+
+    fn generate_signed_url(
+        &self,
+        method: hyper::Method,
+        endpoint: &str,
+        parameters: &str,
+        body: &str,
+    ) -> String {
+        self.url_with_nonce_and_timestamp(
+            method,
+            endpoint,
+            parameters,
+            body,
+            helper::nonce(),
+            helper::now(),
+        )
+    }
+
+    fn url_with_nonce_and_timestamp(
+        &self,
+        method: hyper::Method,
+        endpoint: &str,
+        parameters: &str,
+        body: &str,
+        nonce: uuid::Uuid,
+        timestamp: u64,
+    ) -> String {
+        let url = format!(
+            "{}{}?apikey={}&nonce={}&timestamp={}{}{}",
+            self.url,
+            endpoint,
+            self.key,
+            nonce,
+            timestamp,
+            if !parameters.is_empty() { "&" } else { "" },
+            parameters
+        );
+
+        let salted_msg = format!("{}\0{}\0{}", method, url, body);
+        format!("{}&signature={}", url, helper::hmac_sha256(&self.shared_secret, &salted_msg))
+    }
 }
